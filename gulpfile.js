a5cde774251f9af8cdb625e6c70e1fa9017f7240
Street Money <Street Money>
2016-08-27 12:56:27 +0300

    Added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
     concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
     gutil = require('gulp-util');
     
 var coffeeSources = [
     'components/coffee/tagline.coffee'
+];
+
+var sassSources = [
+    'components/sass/style.scss'
 ];
 
 var jsSources = [
@@ -27,4 +32,15 @@ gulp.task('js', function () {
             .pipe(concat('script.js'))
             .pipe(browserify())
             .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function () {
+    gulp.src(sassSources)
+            .pipe(compass({
+                sass: 'components/sass',
+                image: 'builds/development/images',
+                style: 'expanded'
+            }))
+            .on('error', gutil.log)
+            .pipe(gulp.dest('builds/development/css'));
 });
