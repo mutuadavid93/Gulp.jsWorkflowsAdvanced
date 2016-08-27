0e4b31a151390e97fe6334cfb14df3b3c70d4382
Street Money <Street Money>
2016-08-27 12:10:03 +0300

    Added coffeescript file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp'),
+    coffee = require('gulp-coffee'),
     gutil = require('gulp-util');
+    
+var coffeeSources = [
+    'components/coffee/tagline.coffee'
+];
 
-gulp.task('log', function() {
-    gutil.log("Workflows are Kinda awesome!!");
+gulp.task('coffee', function() {
+    gulp.src(coffeeSources)
+            .pipe(coffee({ bare: true }))
+            .on('error', gutil.log)
+            .pipe(gulp.dest('components/scripts'));
 });
